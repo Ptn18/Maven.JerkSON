5ff78e8380151a0d91e89105e5d384b3dd4dc248
Thao Nguyen <thaonguyen@zipoders-mbp.lan>
2019-03-18 23:51:49 -0400

    Parser

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,6 +1,12 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GroceryReporter {
     private final String originalFileText;
@@ -9,8 +15,34 @@ public class GroceryReporter {
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
+
     @Override
     public String toString() {
-        return null;
+        ItemParser parser = new ItemParser();
+        List<Item> itemsList = parser.parseItemList(originalFileText);
+        Map<String, List<Item>> itemsMap = createItemMap(itemsList);
+
+        StringBuilder builder = new StringBuilder("name    " + itemsList);
+        itemsMap.toString();
+        System.out.println(builder);
+
+        return builder.toString();
     }
+
+    private Map<String, List<Item>> createItemMap(List<Item> itemsList) {
+        Map<String, List<Item>> itemsMap = new HashMap<>();
+
+        for (int i = 0; i < itemsList.size(); i++) {
+            Item item = itemsList.get(i);
+            List<Item> list = itemsMap.get(item.getName());
+            if(list == null){
+                list = new ArrayList<>();
+            }
+            list.add(item);
+            itemsMap.put(item.getName(), list);
+        }
+        return itemsMap;
+    }
+
+
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,42 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        String[] noP = valueToParse.split("##");
+        List<Item> items = new ArrayList<>();
+        for(String string: noP){
+            try {
+                items.add(parseSingleItem(string));
+            } catch (ItemParseException e) {
+//                e.printStackTrace();
+            }
+        }
+
+        return items;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+
+        String lowerSingle = singleItem.toLowerCase().replace("##","");
+        String[] keyAndValue = semiparse(lowerSingle);
+        Map<String, String> items = new TreeMap<>();
+        for (String string : keyAndValue) {
+            String[] moreSplitting = string.split("[:%^*!@;]");
+            if(moreSplitting.length < 2) {
+                throw new ItemParseException();
+            }
+            items.put(moreSplitting[0], moreSplitting[1]);
+        }
+
+        return new Item(items.get("name"),Double.parseDouble(items.get("price")),items.get("type"),items.get("expiration"));
+    }
+    public String[] semiparse(String withSemi){
+        return withSemi.split(";");
     }
 }
